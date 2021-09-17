ffb4c928c4b859a37ef182ea94931ccedab05cb1
Nicholas-Darko-Brown <nicholas.brown@amalitech.org>
2021-09-17 21:47:01 +0000

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
+import { animals } from './animals';
+import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const title = "";
+
+  const showBackground = true;
+  
+  const background = (
+    <img 
+      className="background"
+      alt="ocean"
+      src='/images/ocean.jpg'
+    />
   );
+  
+  const images = [];
+  for (const animal in animals){
+    images.push(
+      <img 
+        key = {animal}
+        className = 'animal'
+        alt = {animal}
+        src = {animals[animal].image}
+        ariaLabel = {animal}
+        role = 'button'
+        onClick = {displayFact}
+      />
+    )
+  }
+  
+  function displayFact (e){
+    const selectedAnimal = e.target.alt;
+    const animalInfo = animals[selectedAnimal];
+    const optionIndex = Math.floor(Math.random() * animalInfo.facts.length);
+  
+    const funfact = animalInfo.facts[optionIndex];
+    document.getElementById('fact').innerHTML = funfact;
+  }
+  
+  return (
+      <div>
+        <h1>{title || "Click an animal for a fun fact"}</h1>
+        {showBackground && background}
+        <p id= 'fact'></p>
+        <div className = 'animals'>
+          {images}
+        </div>
+      </div>
+    );
 }
 
 export default App;
